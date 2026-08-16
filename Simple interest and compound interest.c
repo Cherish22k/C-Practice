f05c160e99130756ce64d7e7f4562b7bf631cad1
Cherish Sharma <cherish.sharma2007@gmail.com>
2026-08-16 15:19:46 +0530

    Update Simple interest and compound interest.c

--- Simple interest and compound interest.c
+++ Simple interest and compound interest.c
@@ -8,7 +8,7 @@ void main()
 {
 float p,r,t,si,ci;
 printf("\n Enter the value of the Principle, Rate , Time:");
-scanf("%f%f%f" %p%r%t);
+scanf("%f%f%f" %p%r%t);     
 si=(p*r*t)/100;
 ci=p*pow(1+r/100),t)-p;
 printf("\n Simple interest is = %f" , si);
