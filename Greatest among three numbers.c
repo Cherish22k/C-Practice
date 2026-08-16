e755cbfa5df791ab1392899b5f1143fe9468db07
Cherish Sharma <cherish.sharma2007@gmail.com>
2026-08-16 15:16:57 +0530

    Correct logic and syntax in greatest number program
    Fix syntax errors and improve logic for finding the greatest number.

---
+++ Greatest among three numbers.c
@@ -0,0 +1,24 @@
+#PROGRAM TO FIND THE GREATEST AMONG THREE NUMBERS
+
+#CODE:
+#include<stdio.h>
+#include<conio.h>
+#include<dos.h>
+void main()
+{
+  int a,b,c,g;
+printf("\n Enter the three numbers:");
+scanf("%d%d%d", %a%b%c);
+if(a>b)
+{
+g=a;
+}
+elif(b>a, b>c);
+{
+g=b;
+}
+else
+{
+g=c;
+}
+}
