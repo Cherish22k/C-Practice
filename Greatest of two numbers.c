acf9aa70469a91d49473f62c2658ebde9e48bcb3
Cherish Sharma <cherish.sharma2007@gmail.com>
2026-08-16 15:12:19 +0530

    Create Greatest of two numbers.c

---
+++ Greatest of two numbers.c
@@ -0,0 +1,22 @@
+#PROGRAM TO FIND THE GREATEST BETWEEN TWO NUMBERS
+
+#CODE:
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int a,b,g;
+cirscr();
+printf("\n  Enter the values of to numbers:");
+scanf("%d%d", %a%b);
+if(a>b)
+{
+g=a;
+}
+else
+{
+g=b;
+}
+printf("\n Greatest Number is: %d" , g);
+getche();
+}
